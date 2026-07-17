2898574c84866b80f92d71354fb2fb386b3c1f6b
Filip Norlin <filip.norlin@proton.me>
2026-07-18 00:07:10 +0200

    Resolve kicad error

--- scripts/kicad.sh
+++ scripts/kicad.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-echo "[6/8] Installing KiCad 10..."
+echo "Installing KiCad..."
 
+PPA="ppa:kicad/kicad-10.0-releases"
 
-sudo add-apt-repository --yes ppa:kicad/kicad-10-releases
+if ! grep -Rq "kicad-10.0-releases" /etc/apt/sources.list /etc/apt/sources.list.d 2>/dev/null; then
+    sudo add-apt-repository --yes "$PPA"
+else
+    echo "KiCad repository already exists."
+fi
 
 sudo apt update
-
-
-sudo apt install -y \
-kicad \
-kicad-packages3d
-
-
-echo "KiCad installed"
+sudo apt install --install-recommends -y kicad
